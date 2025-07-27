5197367decc4cdd93a38c33ef9adc2fba1bfe7b1
jliobarreto <julioc.barretoq@outlook.com>
2025-07-26 19:00:37 -0500

    V1.0.0.12

--- logic/memory.py
+++
@@ -1,5 +0,0 @@
-"""Memoria en ejecución para registrar operaciones enviadas."""
-
-# Diccionario para almacenar operaciones enviadas pero aún no confirmadas.
-# La clave suele ser el símbolo de la operación.
-operaciones_enviadas = {}
